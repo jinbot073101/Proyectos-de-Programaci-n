e8f2e2780c59bc6c8d5dc82f3cb12b5908069780
jinB0 <166962541+jinbot073101@users.noreply.github.com>
2024-09-03 12:20:20 -0500

    Delete TallerAvanzada1 directory

--- TallerAvanzada1/Reporte.java
+++
@@ -1,9 +0,0 @@
-public class Reporte {
-
-    public void generarReporte(){
-
-        
-        System.out.println("Gracias");
-    }
-
-}
--- TallerAvanzada1/TallerAvanzada1/ControlInterface.java
+++
@@ -1,8 +0,0 @@
-public interface ControlInterface {
-    public void ingresarPersona(int cedula);
-
-    public void consultarPersona();
-
-    public void cambiarSituacion();
-
-}
